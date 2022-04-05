661ca4da44702df64371379fcfdafae9d391f493
HowLemon <77616267+HowLemon@users.noreply.github.com>
2022-04-05 15:06:48 +0800

    more practice

--- src/practice.js
+++ src/practice.js
@@ -5,23 +5,76 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./practice.css";
 
 class Board extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xTurn: true
+        }
+    }
+
     renderSquare(i) {
         return (
-            <Square key={Math.random()} value={i} />
+            <Square key={`sq${i}`} value={this.state.squares[i]} onClick={()=>this.clickSquare(i)}/>
         );
+    }
+
+    clickSquare(i) {
+        // console.log(i)
+        const squares = this.state.squares.slice();
+        if(squares[i]) return;
+        squares[i] = this.state.xTurn ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xTurn: !this.state.xTurn
+        })
+        let result = this.caculateWinner(squares);
+        if(result){
+            alert(result + " wins!")
+        }
+    }
+
+    caculateWinner(squares){
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+        let result = null;
+        lines.forEach(([a,b,c]) => {
+            if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c] && squares[a] === squares[c]){
+                result = squares[a];
+            }
+        });
+        return result;
     }
 
 
 
     render() {
-        const status = "Next Player: X";
-        const size = 3
+        
+        
         return (
             <div>
-                <div className="status">{status}</div>
+                <div className="status">{"Player: " + (this.state.xTurn ? 'X' : 'O')}</div>
                 <div className="board-container">
                     <div className="board">
-                        {[...Array(size * size)].map((x, i) => this.renderSquare(i + 1))}
+                        {/* {[...Array(9)].map((x, i) => this.renderSquare(this.state.squares[i]))} */}
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
                     </div>
                 </div>
             </div>
@@ -37,7 +90,7 @@ class Square extends React.Component {
 
     render() {
         return (
-            <button className="square btn border" onClick={this.buttonClicked}>
+            <button className="square btn border" onClick={()=>this.props.onClick()}>
                 {this.props.value}
             </button>
         )
